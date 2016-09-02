c352b1bcd0c128e2993a9a23f46fa7b8ad22d09f
Shmuela <shmuela@500tech.com>
2016-09-02 17:15:57 +0300

    module: added missing './'

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './components/app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
-import { TodoItemComponent } from 'components/todo-item/todo-item.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { TodoInputComponent } from './components/todo-input/todo-input.component';
 
